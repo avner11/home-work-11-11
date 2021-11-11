725a9969cc2b31a6061cddf03e28fce9cc6884ab
avner11 <92875541+avner11@users.noreply.github.com>
2021-11-11 21:15:11 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+  /* 1    for(int i=20; i<=2000;i=i+5){
+          System.out.println(i);
+      }*/
+
+   /* 2     int sumTotal=0;
+        for(int i=1;i<11;i++){
+            sumTotal= sumTotal+i;
+            System.out.println(String.format("sum now = %d",sumTotal));
+        }
+        System.out.println(String.format("the total is: %d",sumTotal));*/
+
+        /* 3 for(int i=5; i>=-10;i--) {
+            System.out.println(i);*/
+
+
+       /* 4  for(int i=1; i<=100;i++){
+           if(i%7==0){
+            System.out.println(i);}
+        }*/
+
+        /* 5 for(int i=1; i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);}
+        }*/
+
+       /*6  for(int i=1; i<=100;i++){
+            if(i%2 != 0){
+                System.out.println(i);}
+        }*/
+
+        // 5 and 7 are the same
+        // 6 and 8 are the same
+
+       /* 9 int totalCheck=0;
+        System.out.println("input number from 1-100:");
+        int inputNum= scanner.nextInt();
+        for(int i=1;i<100;i++){
+            totalCheck = totalCheck + inputNum;
+            if(totalCheck>100){
+                System.out.println("done");
+                break;
+            }else{
+                System.out.println("input number from 1-100:");
+                inputNum= scanner.nextInt();*/
+            }
+        }
+    }
+}
+
+
+
